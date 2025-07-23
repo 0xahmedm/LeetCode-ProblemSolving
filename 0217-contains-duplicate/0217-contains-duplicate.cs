603e9c617de6ef555459aee738b5cd066319fb4c
AhmedM03 <104654023+0xahmedm@users.noreply.github.com>
2025-07-23 14:16:35 +0300

    Time: 87 ms (9.21%), Space: 58.5 MB (91.2%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -1,6 +1,14 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        HashSet<int> unique = new HashSet<int>(nums);
-        return unique.Count == nums.Length ? false : true;
+        Array.Sort(nums);
+        int n = nums.Length;
+        for (int i = 1; i<n;i++){
+            if (nums[i]==nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+        // HashSet<int> unique = new HashSet<int>(nums);
+        // return unique.Count==nums.Length?false:true;
     }
 }
