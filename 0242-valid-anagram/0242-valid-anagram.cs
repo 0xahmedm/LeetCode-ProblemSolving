ef25ae34eba5fdf34e467b11446fb5a6e6360b5b
AhmedM03 <104654023+0xahmedm@users.noreply.github.com>
2025-07-23 15:18:23 +0300

    Time: 5 ms (70.68%), Space: 43.9 MB (55.41%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -0,0 +1,49 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        Dictionary<char, int> sCounts = new Dictionary<char, int>();
+    Dictionary<char, int> tCounts = new Dictionary<char, int>();
+    foreach (char c in s)
+    {
+        if (!sCounts.ContainsKey(c))
+        {
+            sCounts.Add(c, 1);
+        }
+        else
+        {
+            sCounts[c]++;
+        }
+    }
+
+    foreach (char c in t)
+    {
+        if (!tCounts.ContainsKey(c))
+        {
+            tCounts.Add(c, 1);
+        }
+        else
+        {
+            tCounts[c]++;
+        }
+    }
+
+if (sCounts.Count != tCounts.Count)
+    {
+        return false;
+    }
+    foreach (KeyValuePair<char, int> kvp in sCounts)
+    {
+        if (!tCounts.ContainsKey(kvp.Key))
+        {
+            return false;
+        }
+
+        if (kvp.Value != tCounts[kvp.Key])
+        {
+            return false;
+        }
+
+    }
+
+    return true;
+    }
+}
